f54c60147c833187071f4ee8ef4b5f303e1ba42b
DastanDev <dstnmhmd@gmail.com>
2021-09-25 19:53:41 +0300

    update

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -3,7 +3,7 @@ module.exports = {
     {
       name: "seldom-page",
       script:
-        "node node_modules/next/dist/bin/next build && node node_modules/next/dist/bin/next start",
+        "node node_modules/next/dist/bin/next build && node node_modules/next/dist/bin/next start -p 2000",
       env: {
         NODE_ENV: "development",
       },
